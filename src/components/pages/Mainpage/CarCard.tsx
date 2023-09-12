7a91e502caa06c7161f9c51aa8581fd25d6f2ea8
talhache <adam_ibragimov_1996@mail.ru>
2023-09-12 12:17:23 +0300

    Стилизовал карточки моделей, настроил Линк, есть кнопка перехода к конкретной модели

--- src/components/pages/Mainpage/CarCard.tsx
+++ src/components/pages/Mainpage/CarCard.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styles from './Mainpage.module.css'
+import {  Link } from 'react-router-dom'
 
 interface CarCardProps {
+  carsId: string,
   img: string;
   name: string;
   price: string;
@@ -9,14 +11,16 @@ interface CarCardProps {
   capacity: number;
 }
 
-const CarCard: React.FC<CarCardProps> = ({ img, name, price, description, capacity }) => {
+const CarCard: React.FC<CarCardProps> = ({ img, name, price, description, capacity, carsId }) => {
+  const shortDescription = description.split(' ').slice(0, 10).join(' ') + '...';
   return (
-    <div className={styles.CarCard}>
-      <img src={`http://localhost:4444/assets/img/${img}`} alt={name} />
-      <h2>{name}</h2>
-      <p>Price: {price}</p>
-      <p>{description}</p>
-      <p>Capacity: {capacity}</p>
+    <div className={styles.carCard}>
+      <div><img className={styles.imgCar} src={`http://localhost:4444/assets/img/${img}`} alt={name} /></div>
+      <div><h2>{name}</h2></div>
+      <div>Price: {price}</div>
+      <div>{shortDescription}</div>
+      <div>Capacity: {capacity}</div>
+      <Link to={`/cars/${carsId}`} className={styles.link}>Перейти к модели</Link>
     </div>
   );
 }
