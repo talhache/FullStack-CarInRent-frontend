b3fb3c1d56beb4ff568d264e57ee3519a38e392d
Mokhamed <gari142@mail.ru>
2023-09-14 12:51:43 +0300

    Сделал вывод текста

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,10 @@ import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>)
+)
