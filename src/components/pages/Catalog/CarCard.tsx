2508bf3226c9043fa6149bbf70ee7b8f432054fb
talhache <adam_ibragimov_1996@mail.ru>
2023-09-14 11:41:11 +0300

    fix catalog

--- src/components/pages/Catalog/CarCard.tsx
+++ src/components/pages/Catalog/CarCard.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import styles from './Catalog.module.css'
 import { Link } from 'react-router-dom'
 import CompareIcon from '@mui/icons-material/Compare';
-import { Tooltip, IconButton } from '@mui/material';
+import { Tooltip, IconButton, Grid } from '@mui/material';
+import CarRentalIcon from '@mui/icons-material/CarRental';
+
 
 
 interface CarCardProps {
@@ -14,7 +16,11 @@ interface CarCardProps {
   capacity: number;
 }
 
-const handleCompareClick = (e) => {
+const handleCompareClick = (e: React.ChangeEvent<HTMLInputElement>) => {
+  e.stopPropagation();
+}
+
+const handleRentClick = (e: React.ChangeEvent<HTMLInputElement>) => {
   e.stopPropagation();
 }
 
@@ -24,24 +30,36 @@ const CarCard: React.FC<CarCardProps> = ({ img, name, price, description, capaci
 
   return (
     <>
-      <div className={styles.carCard}>
-        <div><img className={styles.imgCar} src={`http://localhost:4444/assets/img/${img}`} alt={name} /></div>
-        <div className={styles.info}>
+      <Grid className={styles.carCard}>
+        <Grid item xs={12} sm={4}>
+          <img className={styles.imgCar} src={`http://localhost:4444/assets/img/${img}`} alt={name} />
+        </Grid>
+        <Grid item xs={12} sm={8} className={styles.info}>
           <div><h2>{name}</h2></div>
           <div>Price: {price}</div>
           <div>{shortDescription}</div>
           <div>Capacity: {capacity}</div>
-        </div>
+        </Grid>
         <div className={styles.compareIconContainer}>
-          <Tooltip  title="Сравнить" placement="top">
-            <IconButton onClick={handleCompareClick}>
-              <CompareIcon className={styles.compareIcon} />
-            </IconButton>
-          </Tooltip>
+          <Link to={`/cars/${carsId}`}>
+            <Tooltip title="Сравнить" placement="top">
+              <IconButton >
+                <CompareIcon className={styles.compareIcon} />
+              </IconButton>
+            </Tooltip>
+          </Link>
+
+          <Link to={`/cars/${carsId}`}>
+            <Tooltip title="Арендовать" placement="top">
+              <IconButton >
+                <CarRentalIcon className={styles.compareIcon} />
+              </IconButton>
+            </Tooltip>
+          </Link>
         </div>
         <Link to={`/cars/${carsId}`} className={styles.link}>Перейти к модели</Link>
 
-      </div>
+      </Grid>
     </>
   );
 }
